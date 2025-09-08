8c766b142bf615b4c84e43a3a50a2a3003dd3a46
geonjineer <atoo1520@gmail.com>
2025-09-08 18:39:01 +0900

    feat: Update import java.util.list in FileRepository.java

--- src/main/java/com/sprint/project/hrbank/repository/FileRepository.java
+++ src/main/java/com/sprint/project/hrbank/repository/FileRepository.java
@@ -1,8 +1,10 @@
 package com.sprint.project.hrbank.repository;
 
 import com.sprint.project.hrbank.entity.File;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FileRepository extends JpaRepository<File, Long> {
 
+  List<File> id(Long id);
 }

93476db270a3e67079edd557a3d1725ec1104908
geonjineer <atoo1520@gmail.com>
2025-09-09 14:20:14 +0900

    feat: File.java 컬럼 설정

--- src/main/java/com/sprint/project/hrbank/entity/File.java
+++ src/main/java/com/sprint/project/hrbank/entity/File.java
@@ -1,34 +1,41 @@
 package com.sprint.project.hrbank.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "files")
+@Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-
-@RequiredArgsConstructor
-@Getter
-@Setter
-@Builder
-
+@EntityListeners(AuditingEntityListener.class)
 public class File {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Column(name = "file_name", length = 255, nullable = true)
   private String fileName;
+
+  @Column(name = "content_type", length = 100)
   private String contentType;
+
+  @Column(nullable = true)
   private long size;
 }

811465f08fae32ed8d1a4905b2af185b5c9f0c82
김규섭 <pens9053@gmail.com>
2025-09-09 21:17:52 +0900

    fix: 빌드 과정에서 문제를 발생시키던 테스트 메서드 제거

--- src/test/java/com/sprint/project/hrbank/HrbankApplicationTests.java
+++ src/test/java/com/sprint/project/hrbank/HrbankApplicationTests.java
@@ -1,13 +1,8 @@
 package com.sprint.project.hrbank;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HrbankApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }

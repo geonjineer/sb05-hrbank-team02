03805aef60ef6d8435956ccc444e5d75d01b837d
geonjineer <atoo1520@gmail.com>
2025-09-09 14:27:44 +0900

    feat: File.java 컬럼 수정

--- src/main/java/com/sprint/project/hrbank/HrbankApplication.java
+++ src/main/java/com/sprint/project/hrbank/HrbankApplication.java
@@ -1,13 +1,15 @@
 package com.sprint.project.hrbank;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class HrbankApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HrbankApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(HrbankApplication.class, args);
+  }
 
 }

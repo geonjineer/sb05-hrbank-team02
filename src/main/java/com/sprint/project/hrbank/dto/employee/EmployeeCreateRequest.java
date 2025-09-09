0e7ada94025ec78c242753686c0e2f10db96e102
woohyuk99 <qusdngur1008@gmail.com>
2025-09-09 16:17:01 +0900

    feat : Test Commit_employee-update1

---
+++ src/main/java/com/sprint/project/hrbank/dto/employee/EmployeeCreateRequest.java
@@ -0,0 +1,17 @@
+package com.sprint.project.hrbank.dto.employee;
+
+import java.time.LocalDate;
+import lombok.Builder;
+
+@Builder
+
+public record EmployeeCreateRequest(
+    String name,
+    String email,
+    long departmentId,
+    String position,
+    LocalDate hireDate,
+    String memo
+) {
+
+}

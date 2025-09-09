0f556ae19f83db602ea67d530dc377cc7cbfbfd4
geonjineer <atoo1520@gmail.com>
2025-09-09 14:35:16 +0900

    feat: FileRepository.java and HrbankApplication.java 사용안하는 import 삭제

--- src/main/java/com/sprint/project/hrbank/repository/FileRepository.java
+++ src/main/java/com/sprint/project/hrbank/repository/FileRepository.java
@@ -1,7 +1,6 @@
 package com.sprint.project.hrbank.repository;
 
 import com.sprint.project.hrbank.entity.File;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FileRepository extends JpaRepository<File, Long> {

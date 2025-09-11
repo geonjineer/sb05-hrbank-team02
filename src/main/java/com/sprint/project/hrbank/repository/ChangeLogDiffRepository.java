2040cf096b07a3b44e02f4225cfdb7aadeb1e6b5
김규섭 <pens9053@gmail.com>
2025-09-11 22:46:41 +0900

    fix: 사용하지 않는 레포지토리 파일 제거

--- src/main/java/com/sprint/project/hrbank/repository/ChangeLogDiffRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sprint.project.hrbank.repository;
-
-import com.sprint.project.hrbank.entity.ChangeLogDiff;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ChangeLogDiffRepository extends CrudRepository<ChangeLogDiff, Long> {
-
-}

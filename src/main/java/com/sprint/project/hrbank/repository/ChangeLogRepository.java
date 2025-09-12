2d6bb7340028e95985abe8a6f7918406cc441f8d
geonjineer <atoo1520@gmail.com>
2025-09-12 14:16:09 +0900

    feat: ChangeLog 레포지토리 및 변경 로그 조회 메서드 추가

--- src/main/java/com/sprint/project/hrbank/repository/ChangeLogRepository.java
+++ src/main/java/com/sprint/project/hrbank/repository/ChangeLogRepository.java
@@ -1,13 +1,17 @@
 package com.sprint.project.hrbank.repository;
 
+import java.time.Instant;
 import java.time.OffsetDateTime;
 import com.sprint.project.hrbank.entity.ChangeLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
 
 public interface ChangeLogRepository extends JpaRepository<ChangeLog, Long>,
     ChangeLogQueryRepository {
   @Query("select count (c) from ChangeLog c where c.at > :base")
   Long countChangeAfter(OffsetDateTime base);
+  boolean existsByAtAfter(@NonNull Instant time);
 
 }
+
